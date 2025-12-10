38a4c1c02817aa948f883ee21f7393ac1687cf14
AlejandroLilloRodriguez <alejandrolillo.rodriguez@usp.ceu.es>
2025-12-10 11:27:20 +0100

    clase reserva casi terminada

--- parking/reservas/models.py
+++ parking/reservas/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from usuarios.models import Usuario
+from plazas.models import Plaza
+
+
+class Estado(models.TextChoices):
+    RESERVADA = "RESERVADA", "Reservada"
+    OCUPADA = "OCUPADA", "Ocupada en plaza"
+    FINALIZADA = "FINALIZADA", "Finalizada"
+    CANCELADA = "CANCELADA", "Cancelada"
+    NO_SHOW = "NO_SHOW", "No se presentó"
 
 class Reserva(models.Model):
-    pass
+    fechaInicio = models.DateTimeField()
+    fechaFinal = models.DateTimeField()
+    estado = models.CharField(max_length=20,choices=Estado.choices,default=Estado.RESERVADA)
+    usuario = models.ForeignKey()
+    plaza = models.ForeignKey()
 # Create your models here.
