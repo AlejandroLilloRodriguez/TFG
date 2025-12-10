90a59eacfa484a14a488c01ce44a4a54ed8f31e4
AlejandroLilloRodriguez <alejandrolillo.rodriguez@usp.ceu.es>
2025-12-10 11:13:28 +0100

    clases planta y plaza completadas

--- parking/plazas/models.py
+++ parking/plazas/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
-class Plaza(models.Model):
-    pass
-
 class Planta(models.Model):
-    pass
+    nivel = models.IntegerField()
+    numeroPlazas = models.IntegerField()
+    
+    def __str__(self):
+        return self.nivel
+    
+class Plaza(models.Model):
+    numero = models.IntegerField()
+    disponible = models.BooleanField(default= True)
+    planta = models.ForeignKey(Planta,on_delete=models.CASCADE)
+    
+    class Meta:
+        unique_together = ('numero', 'planta')
+    
+    def __str__(self):
+        return f"Plaza {self.numero} - {self.planta.nivel}"
 # Create your models here.
