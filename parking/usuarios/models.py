e2655025cd7556537185d191141cdb1d584f3f0d
AlejandroLilloRodriguez <alejandrolillo.rodriguez@usp.ceu.es>
2025-12-10 10:46:08 +0100

    clase usuario completada

--- parking/usuarios/models.py
+++ parking/usuarios/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class Usuario(models.Model):
-    pass
+class Usuario(AbstractUser):
+    class Roles(models.TextChoices):
+        Admin = "ADMIN","Administrador"
+        Empleado = "EMPLEADO","Empleado"
+    
+    name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    contraseña = models.CharField(max_length=128)
+    rol = models.CharField(max_length=10, choices=Roles.choices, default=Roles.Empleado)
+
+    
     
 class Vehiculo(models.Model):
     pass
