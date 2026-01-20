761f69aa419569bc92286e424e99631d73aa486f
AlejandroLilloRodriguez <alejandrolillo.rodriguez@usp.ceu.es>
2026-01-20 13:57:15 +0100

    endpoint vehiculos

---
+++ parking/vehiculo/urls.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import DefaultRouter
+from .views import VehiculoViewSet
+
+router = DefaultRouter()
+router.register(r"vehiculos", VehiculoViewSet, basename="vehiculos")
+urlpatterns = router.urls
--- parking/vehiculo/views.py
+++ parking/vehiculo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Vehiculo
+from .serializer import VehiculoSerializer
+
+class VehiculoViewSet(viewsets.ModelViewSet):
+    serializer_class = VehiculoSerializer
+    def get_queryset(self):
+        if getattr(self.request.user, "rol", None) == "ADMIN":
+            return Vehiculo.objects.all()
+        else:
+            return Vehiculo.objects.filter(usuario=self.request.user)
+        
+    def perform_create(self, serializer):
+        serializer.save(usuario=self.request.user)
 
 # Create your views here.
